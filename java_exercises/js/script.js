159c2970e90593bd91c4510746fc19201e09b89c
MyNameJaeff <fredrik.andersson@elev.ga.ntig.se>
2023-09-26 11:47:59 +0200

    done for now

---
+++ java_exercises/js/script.js
@@ -0,0 +1,73 @@
+// Hashtag Pyramid
+// var hash = "";
+// for(let i = 1; i <= 7; i++){
+//     hash += "#"
+//     console.log(hash);
+// }
+
+// // Fizz Buzz
+// for(let i = 1; i <= 100; i++){
+//     if(i % 3 == 0){
+//         console.log("Fizz");
+//     }else if(i % 5 == 0){
+//         console.log("Buzz");
+//     }else{
+//         console.log(i);
+//     }
+// }
+
+// Chess board
+// var string = "";
+// for (let i = 1; i <= 8; i++) {
+//   if (i % 2 == 0) {
+//     string += " # # # #\n";
+//   } else {
+//     string += "# # # # \n";
+//   }
+// }
+// console.log(string);
+
+// Min
+// const min = (a,b) => {
+//     if(a < b){
+//         return a;
+//     }else{
+//         return b;
+//     }
+// }
+// console.log(min(0, 10));
+// console.log(min(0, -10));
+
+// Is even?
+// var isEven = x => {
+//     if(x % 2 == 0){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+// console.log(isEven(50));
+// console.log(isEven(75));
+// console.log(isEven(-2));
+
+// Counting strings
+// var countBs = x => {
+//     var count = 0;
+//     for(let i = 0; i < x.length; i++){
+//         if(x[i] == "b" || x[i] == "B"){
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// var countChar = (x,y) => {
+//     var count = 0;
+//     for(let i = 0; i < x.length; i++){
+//         if(x[i] == y){
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// console.log(countBs("BBC"));
+// console.log(countChar("kakkerlak", "k"));
