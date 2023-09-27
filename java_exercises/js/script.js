0c17bc6948aaf534f70faad355b4ee39b8125a45
MyNameJaeff <fredrik.andersson@elev.ga.ntig.se>
2023-09-27 14:59:28 +0200

    a

--- java_exercises/js/script.js
+++ java_exercises/js/script.js
@@ -1,3 +1,4 @@
+/*
 //Hashtag Pyramid
 var hash = "";
 for (let i = 1; i <= 7; i++) {
@@ -75,3 +76,110 @@ var countChar = (x, y) => {
 };
 console.log(countBs("BBC"));
 console.log(countChar("kakkerlak", "k"));
+
+*/
+
+const range = (x, y, step = 1) => {
+  let arr = [];
+  if (x < y) {
+    for (x; x <= y; x += step) {
+      arr.push(x);
+    }
+  } else {
+    for (x; x >= y; x += step) {
+      arr.push(x);
+    }
+  }
+  return arr;
+};
+const sum = (array) => {
+  let tot = 0;
+  array.forEach((element) => {
+    tot += element;
+  });
+  return tot;
+};
+console.log(range(1, 10));
+console.log(range(5, 2, -1));
+console.log(sum(range(1, 10)));
+
+const reverseArray = (x) => {
+  let newArr = [];
+  let i = 0;
+  for (x.length; i < x.length; ) {
+    newArr.push(x.pop());
+  }
+  return newArr;
+};
+const reverseArrayInPlace = (x) => {
+  let i = 0;
+  for (x.length; i < Math.floor(x.length / 2); ) {
+    let temp = x[i];
+    x[i] = x[x.length - 1 - i];
+    x[x.length - 1 - i] = temp;
+    i++;
+  }
+  return x;
+};
+console.log(reverseArray(["A", "B", "C"]));
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+
+const arrayToList = (x) => {
+  var list = {};
+  for (let i = 0; x.length > i; i++) {
+    list.value = x.splice(0, 1)[0];
+    list.rest = x.length > 0 ? arrayToList(x) : null;
+  }
+  return list;
+};
+
+const listToArray = (x) => {
+  let arr = [];
+  for (let node = x; node; node = node.rest) {
+    arr.push(node.value);
+  }
+  return arr;
+};
+const prepend = (x, y = {}) => {
+  let list = {};
+  list.value = x;
+  list.rest = y;
+  return list;
+};
+const nth = (x, y) => {
+  let arr = [];
+  for (let node = x; node; node = node.rest) {
+    arr.push(node.value);
+  }
+  return arr[y];
+};
+console.log(arrayToList([1, 2, 3]));
+console.log(listToArray(arrayToList([10, 20, 30])));
+console.log(prepend(10, prepend(20, null)));
+console.log(nth(arrayToList([10, 20, 30]), 1));
+
+
+const deepEqual = (first, second) => {
+  if(first === second){
+    return true;
+  };
+  if(typeof first == "object" && first != null && typeof second == "object" && second != null){
+    let x = Object.keys(first), y= Object.keys(second);
+    if(x.length == y.length){
+      for(key of x){
+        if(y.includes(key)){
+          if(first[key] == second[key]) return true;
+          else return false;
+        }
+      }
+    }
+    return true;
+  }
+}
+
+let obj = { here: { is: "an" }, object: 2 };
+console.log(deepEqual(obj, obj));
+console.log(deepEqual(obj, {here: 1, object: 2}));
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
